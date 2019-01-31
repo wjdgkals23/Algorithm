df10cc478e72ee716a837b80db35f758c14de672
wjdgkals23 <wjdgkals74@gmail.com>
2019-01-31 23:44:43 +0900

    Algorithm study  * programmers 섬연결 문제

--- src/programmers/GreedyConnectIsland.java
+++ src/programmers/GreedyConnectIsland.java
@@ -1,6 +1,7 @@
 package programmers;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class GreedyConnectIsland {
@@ -15,14 +16,20 @@ public class GreedyConnectIsland {
 			this.cost = cost;
 		}
 	}
+	
+	public static int [] parent;
 
 	public static int solution(int n, int[][] costs) {
         int answer = 0;
-        boolean [] visited = new boolean [n+1];
+        parent = new int [n];
+        
+        for(int p=0; p<n; p++) {
+        	parent[p] = p;
+        }
 //        cycle 인지 확인해야하네.... 추가하려는 간선의 꼭지점이 둘 다 true이면 cycle이다!!! 
 //      간선을 오름차순으로 정리하고 가장 처음것부터 하나씩 추가해나가자!!
 //      간선을 추가하면서 섬들을 추가해나갈때 합집합 알고리즘을 이용해야한다.
-//      아직 초기 해결방안에 대해 반례를 찾지 못했다.
+//      아직 초기 해결방안에 대해 반례를 찾지 못했다. -> 간선이 추가되었을 때가 아닌 섬이 추가된 것을 확인하고 추가해야한다.
         LinkedList<Vertex> list = new LinkedList<Vertex>();
         for(int i=0; i<costs.length; i++) {
         	int a = costs[i][0];
@@ -44,21 +51,66 @@ public class GreedyConnectIsland {
 			}
 		});
         
-        boolean flag = true;
-        int cnt = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
         
-        while(flag) {
-        	if(cnt == n-1) break;
+        while(!list.isEmpty()) {
         	Vertex temp = list.removeFirst();
-        	if(visited[temp.start]&&visited[temp.end]) continue;
-        	visited[temp.start] = true;
-        	visited[temp.end] = true;
-        	cnt++;
-        	answer += temp.cost;
+        	int start = temp.start;
+        	int end = temp.end;
+        	if(!set.contains(end)&&!set.contains(start)) {
+        		set.add(end);
+        		set.add(start);
+        		union(end,start);
+        		showparent();
+        		answer+=temp.cost;
+        	}
+        	else if(!set.contains(end)&&set.contains(start)) { // start는 이미 군집에 포함되어있는 경우 
+        		set.add(end);
+        		union(end,start);
+        		showparent();
+        		answer+=temp.cost;
+        	}
+        	else if(set.contains(end)&&!set.contains(start)) { // start는 이미 군집에 포함되어있는 경우 
+        		set.add(start);
+        		union(end,start);
+        		showparent();
+        		answer+=temp.cost;
+        	}
+        	else { // 이미 같은 싸이클에 있는 경우와 서로 다른 싸이클에 포함되어있는 경우로 나뉜다. 
+        		if(findparent(end) == findparent(start)) {
+        			continue;
+        		}
+        		else {
+        			union(end,start);
+        			showparent();
+        			answer+=temp.cost;
+        		}
+        	}
         }
         
         return answer;
     }
+	
+	public static void showparent() {
+		for(int i=0; i<parent.length; i++) {
+			System.out.print(parent[i]);
+		}
+		System.out.println();
+	}
+	
+	public static int findparent(int n) {
+		return parent[n]==n ? n : findparent(parent[n]);
+	}
+	
+	public static void union(int a, int b) { // 함수 수정 
+		int a_ = findparent(a);
+		int b_ = findparent(b);
+		
+		if(a_<b_) {
+
+		}
+		else parent[a] = b_;
+	}
 	
 	public static void main(String[] args) {
 		int n = 4;
